2aca1fa14365ee63ed19cb43393a466fff1f9fec
Behruzbee <behruz.baxtiyorov.dev@gmail.com>
2026-02-02 15:00:23 +0500

    main

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'orzumed.uz', // 👈 Разрешаем загрузку картинок с этого сайта
-        port: '',
-        pathname: '/wp-content/uploads/**', // Разрешаем папку uploads
-      },
-    ],
-    // Разрешаем SVG (так как логотип в формате .svg)
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'orzumed.uz',
+      pathname: '/wp-content/uploads/**',
+    },
+  ],
+  dangerouslyAllowSVG: true,
+  contentDispositionType: 'attachment',
+  contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+}
+
 };
 
 export default nextConfig;
